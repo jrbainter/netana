570ba32b7f194393fcfc0fef4d617a3238356a87
James Bainter <bainter8326@gmail.com>
2025-06-08 16:16:02 -0700

    corrected entry point in setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
       include_package_data = True,
 #      install_requires = ['python3-tk','python3-matplotlib','python3-pil',
 #	  'python3-pil.imagetk', 'python3-numpy'],
-      entry_points = { "gui_scripts" : ["netana=main"]},
+      entry_points = { "gui_scripts" : ["netana=netana:main"]},
       scripts = ["cp-examples.sh"],
       package_data={'netana': ['examples/*', 'doc/*',
         'netana-3.2.0-md5sums', 'netana.desktop', 'license', 'change.log', 'copyright' ]},
